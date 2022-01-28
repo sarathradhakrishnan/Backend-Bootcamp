5de25003f3da1e6fc150f37d47f4bc6fad6cf1e1
Sarath Radhakrishnan <sarathrk1@outlook.com>
2022-01-28 20:50:54 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const routes = require('./routes/taskRoutes')
+const app = express();
+
+app.engine('hbs', exphbs.engine({extname:'hbs'}));
+app.set('view engine', 'hbs');
+
+app.use('/',routes)
+
+app.listen(5000,()=>console.log("Server Started"))
